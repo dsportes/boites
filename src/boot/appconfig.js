e862cb24a8024e9a1ccc6187d17f63a23690c1d1
dsportes <daniel.sportes@laposte.net>
2021-06-10 16:05:47 +0200

    crypto salts

--- src/boot/appconfig.js
+++ src/boot/appconfig.js
@@ -1,6 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 import { setup } from '../app/util'
+const crypt = require('../app/crypto')
 
 export default boot(async ({ app /*, router, store, Vue */ }) => {
   const gp = app.config.globalProperties
@@ -8,4 +9,7 @@ export default boot(async ({ app /*, router, store, Vue */ }) => {
   gp.$cfg = cfg
   setup(gp, cfg)
   console.log('Build : ' + cfg.build)
+  const salts = (await axios.get('./salts', { responseType: 'arraybuffer' })).data
+  crypt.setSalts(salts)
+  // crypt.test()
 })
